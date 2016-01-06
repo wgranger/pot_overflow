36e485b7bcc628500f43324cab448acc0e3b60f4
Ronny Ewanek <email": null>
2016-01-06 16:03:40 -0600

    add question and answer faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,18 @@
     email: Faker::Internet.email
     password: Faker::Internet.password
     )
+
+  Question.create(
+    title: Faker::Name.title
+    content: Faker::Lorem.sentence
+    author: User.find(rand(1..10))
+    )
+
+  Answer.create{
+    content: Faker::Lorem.paragraph
+    question: Question.find(rand(1..10))
+    author: User.find(rand(1..10))
+  }
+
 end
 
