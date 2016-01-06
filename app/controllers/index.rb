54384f0db0fd9c095c620df69006f807d415a394
wgranger <twgranger@gmail.com>
2016-01-06 16:09:13 -0600

    Create Login and Logout

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,22 @@
 get "/" do
   erb :index
 end
+
+get "/login" do
+  erb :login
+end
+
+post "/login" do
+  user = User.find_by(name: params[:name])
+  if user.authenticate(params[:name], params[:password])
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    @errors = user.errors.full_messages
+    erb :login
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
