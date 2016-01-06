47d71ed2313038fe197c79f0b05ee1af5c720164
Will Granger <twgranger@gmail.com>
2016-01-06 15:08:26 -0600

    add migrations

--- db/migrate/20160106141853_create_votes.rb
+++ db/migrate/20160106141853_create_votes.rb
@@ -1,4 +1,11 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+    create_table :votes do |t|
+      t.integer :votable_id
+      t.string :votable_type
+      t.references :voter
+      t.integer :value
+      t.timestamps null: false
+    end
   end
 end
--- db/migrate/20160106141906_create_comments.rb
+++ db/migrate/20160106141906_create_comments.rb
@@ -1,4 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.integer :commentable_id
+      t.string :commentable_type
+      t.string :content
+      t.references :author
+
+      t.timestamps null: false
+    end
   end
 end
