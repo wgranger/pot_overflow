1d4c72244bbd7838fcb4708a29e36afa83365e74
Michael Jasinski <email": null>
2016-01-06 17:44:36 -0600

    add sessions

--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -1,10 +1,10 @@
 helpers do
-
   def current_user
-    @current_user = User.find_by(id: session[:id])
+    @current_user ||= User.find_by(id: session[:id])
   end
 
-  def logged_in?
+  def current_session?
     session[:user_id]
   end
+
 end
