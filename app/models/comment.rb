c14f7449f26ee14c43ccd73dae0d37b39c2372be
Michael Jasinski <email": null>
2016-01-06 15:14:58 -0600

    add models

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+  belongs_to :author, class_name: "User"
+  belongs_to :commentable, polymorphic: :true
+
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
+end
