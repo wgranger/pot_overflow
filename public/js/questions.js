3651668134352f63e6f680f57e21123e40cabea2
J Shin <jjshin85@gmail.com>
2016-01-07 11:14:41 -0600

    add functionality to buttons

--- public/js/questions.js
+++ public/js/questions.js
@@ -5,17 +5,32 @@ $(document).ready(function() {
       var answer_form = '<form action="questions/<%= @question.id %>/answer/new" method="post" ><input id="authorName" type="text" name="author_name" placeholder="Username"><textarea rows="10" cols="30" name="comment" placeholder="Comment"></textarea><input type="submit" value="Post Answer"></form>';
 
       onButtonClick("#comment_button", "#question", comment_form);
-      onButtonClick("#answer_button", "#question", answer_form);
+      // onButtonClick("#answer_button", "#question", answer_form);
 
 
-  function onButtonClick(button_id, original_element, form)
+  function onButtonClick(button_id, original_element, form, methodType, ajaxUrl, onResult, onError)
   {
     $(button_id).click(function(event)
     {
       event.preventDefault();
       $(this).hide();
       $(original_element).after(form);
-    });
+        $(form).on("submit", function(e){
+          e.preventDefault();
+          console.log("Submit bound to form");
+          // ajaxRequest(methodType, ajaxUrl, onResult, onError);
+        });
 
+    });
+  }
+
+  function ajaxRequest(methodType, ajaxUrl, onResult, onError){
+    $.ajax({
+      method: methodType,
+      url: ajaxUrl,
+      success: onResult,
+      error: onError
+
+    });
   }
 });
