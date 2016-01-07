fe266b5a842647cd44f0e3393ac5b75e60d657e8
Michael Jasinski <majasinski8@gmail.com>
2016-01-06 22:23:36 -0600

    Add title form for a new question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -7,6 +7,11 @@ get '/questions/new' do
   erb :'questions/new'
 end
 
+post '/questions' do
+  @question = Question.new(params[:question])
+  redirect '/profile'
+end
+
 get '/questions/:id' do
 	@question = Question.find(params[:id])
 	erb :'questions/show'
