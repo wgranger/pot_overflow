6100f7b6374031c25ba2d13b16c4e273043a18c9
Shea Munion <sheamunion@gmail.com>
2016-01-06 18:38:21 -0600

    Fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
 10.times do
   User.create(
-    name: Faker::Name.name
-    email: Faker::Internet.email
-    password: Faker::Internet.password
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
     )
-
-  Question.create(
-    title: Faker::Name.title
-    content: Faker::Lorem.sentence
-    author: User.find(rand(1..10))
-    )
-
-  Answer.create{
-    content: Faker::Lorem.paragraph
-    question: Question.find(rand(1..10))
-    author: User.find(rand(1..10))
-  }
-
 end
 
+10.times do
+  Question.create(
+    title: Faker::Name.title,
+    content: Faker::Lorem.sentence,
+    author: User.find(rand(1..10))
+    )
+end
+
+10.times do
+  Answer.create(
+    content: Faker::Lorem.paragraph,
+    question: Question.find(rand(1..10)),
+    author: User.find(rand(1..10))
+    )
+end
+
+10.times do
+  Question.find(rand(1..10)).votes << Vote.create(voter_id: User.find(rand(1..10)).id, value: 1)
+end
