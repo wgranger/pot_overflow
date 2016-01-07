72d7bc9ac2ec7dea655f8f2972a24f76ad7ce86e
J Shin <jjshin85@gmail.com>
2016-01-07 10:00:55 -0600

    add answer and comment form to question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -7,3 +7,12 @@ get '/questions/:id' do
 	@question = Question.find(params[:id])
 	erb :'questions/show'
 end
+
+post 'questions/:id/comment/new' do
+end
+
+post 'questions/:id/answer/new' do
+end
+
+post 'questions/:id/answer/:id/comment' do
+end
---
+++ public/js/questions.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+      var comment_form = '<form action="questions/<%= @question.id %>/comment/new" method="post" ><input id="authorName" type="text" name="author_name" placeholder="Username"><textarea rows="10" cols="30" name="comment" placeholder="Comment"></textarea><input type="submit" value="Post Comment"></form>';
+
+      var answer_form = '<form action="questions/<%= @question.id %>/comment/new" method="post" ><input id="authorName" type="text" name="author_name" placeholder="Username"><textarea rows="10" cols="30" name="comment" placeholder="Comment"></textarea><input type="submit" value="Post Answer"></form>';
+
+      onButtonClick("#comment_button", "#question", comment_form);
+      onButtonClick("#answer_button", "#question", answer_form);
+
+
+  function onButtonClick(button_id, original_element, form)
+  {
+    $(button_id).click(function(event)
+    {
+      event.preventDefault();
+      $(this).hide();
+      $(original_element).after(form);
+    });
+
+  }
+});
