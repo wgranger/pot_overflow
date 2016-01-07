9dbfa5339012143848bfe0795be5744cf9312c4b
Michael Jasinski <majasinski8@gmail.com>
2016-01-07 09:12:50 -0600

    Add new message nav bar option

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -4,12 +4,22 @@ get '/questions' do
 end
 
 get '/questions/new' do
+  # if request.xhr?
   erb :'questions/new'
 end
 
+
 post '/questions' do
+
   @question = Question.new(params[:question])
-  redirect '/profile'
+  @question.author = current_user
+  if @question.save
+    @sucesss =  "Your message has been posted."
+    redirect '/profile'
+  else
+    @fail = "Your message did not post."
+    erb :'questions/new'
+  end
 end
 
 get '/questions/:id' do
