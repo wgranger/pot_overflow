9ea58b5daf6ab4f44a052dac2cd03a0c0135823e
J Shin <jjshin85@gmail.com>
2016-01-07 19:34:44 -0600

    add votability to questions and answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,13 @@ class Answer < ActiveRecord::Base
 
   has_many :comments, as: :commentable
   has_many :votes, as: :voteable
+
+  def vote_count
+    return 0 if self.votes.count == 0
+    total = []
+    self.votes.each do |vote|
+      total << vote.value
+    end
+    total.inject{|sum,x| sum + x }
+  end
 end
--- public/js/voting.js
+++ public/js/voting.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
-  $("#voter").on("click", function(e){
+  $(".voter").on("click", function(e){
+    console.log
     e.preventDefault();
     var url = $(this).attr("href");
     vote("GET", url, onGetSuccess);
+    // id = $(this).parent().attr("id");
+  });
+
+  $(".voter_commentable").on("click", function(e){
+    console.log
+    e.preventDefault();
+    var url = $(this).attr("href");
+    vote("GET", url, onCommentableSuccess);
     // id = $(this).parent().attr("id");
   });
 
@@ -18,6 +27,15 @@ $(document).ready(function() {
 
   function onGetSuccess(response) {
     $("#vote-count").html(response);
+  }
+
+  function onCommentableSuccess(response){
+      var responseJSON = jQuery.parseJSON(response);
+      var votes_total = responseJSON.votes;
+      var answer_id = responseJSON.id;
+      console.log(responseJSON.id);
+      $("#vote-count_" + answer_id).html(votes_total);
+      // console.log(response);
   }
 
 });
